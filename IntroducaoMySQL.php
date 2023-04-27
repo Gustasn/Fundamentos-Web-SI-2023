d9760f05d1eaceedb4db8488662f34cd2e432f78
Gustasn <gustavo.sn@outlook.com.br>
2023-04-27 18:49:57 -0300

    teste

--- IntroducaoMySQL.php
+++ IntroducaoMySQL.php
@@ -10,7 +10,27 @@
     
 
         <?php
-        
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "fundamentosweb";
+
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        // Check connection
+        if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $sql = "INSERT INTO estado (nomeEstado) values ('Sao Paulo')";
+
+        if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully";
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+
+        mysqli_close($conn);
         ?>
 </body>
 </html>
